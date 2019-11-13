a5d1d7f755dd56e54d832170aba5f4cad598921a
RomaChel <romanchelombitko@gmail.com>
2019-11-13 22:24:42 +0200

    Changed assert for PKW

--- src/main/java/pagesPKW/SearchFormCar.java
+++ src/main/java/pagesPKW/SearchFormCar.java
@@ -1,4 +1,4 @@
-package pages;
+package pagesPKW;
 
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
--- src/main/java/pagesPKW/UIutilities.java
+++ src/main/java/pagesPKW/UIutilities.java
@@ -1,4 +1,4 @@
-package pages;
+package pagesPKW;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedCondition;
